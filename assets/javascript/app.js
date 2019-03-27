862b52b22290416b7c31f967352c9ff31517fcd1
Jordynn Otto <j_taylor92@yahoo.com>
2019-03-27 14:03:10 -0700

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,9 @@ $(document).ready(function () {
   // hides timer
   $(".countdown").hide();
 
+  $("#text").hide();
+  $("#correct").hide();
+
   // BEGIN BUTTON
   // variable to hold button
   var button1 = $("<button>");
@@ -73,23 +76,23 @@ $(document).ready(function () {
       incorrect++;
     }
 
-    var answerMessage = ["You know the body!", "Ah - so close!", "Better luck next time"]
+    var answerMessage = ["You know the body!", "Ah - so close!", "Better luck next time."];
 
     var range;
     if (correct < 1) {
       range = 2;
     }
 
-    if (correct > 0 && correct < 2) {
+    else if (correct < 2 && correct > 0) {
       range = 1
     }
 
-    if (correct > 1) {
+    else if (correct > 1) {
       range = 0;
-
-      $("#text").text(answerMessage[range]);
-      $("#correct").text("You answered " + correct + " correctly.");
     }
+
+    $("#text").text(answerMessage[range]);
+    $("#correct").text("You answered " + correct + " out of 2 questions correctly.");
 
   }; check();
 
